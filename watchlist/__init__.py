acfce88002f0efbd6b3315925a1d82b83cb5ab60
zhongping yu <yu-zhongping@outlook.com>
2022-11-11 09:57:00 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -12,10 +12,10 @@ if WIN:
 else:
     prefix = 'sqlite:////'
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 # 注意跟新这里的路径，把app.root_path 添加到 os.path.dirname() 中
 # 以便把文件定位到项目根目录
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
---
+++ wsgi.py
@@ -0,0 +1,13 @@
+import os
+from gevent import pywsgi
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from watchlist import app
+
+
+
