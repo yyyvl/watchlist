af0e1e466004bf07055a4df58d3ae17fb144ca9e
zhongping yu <yu-zhongping@outlook.com>
2022-11-11 16:09:03 +0800

    ready wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
 import os
-from gevent import pywsgi
-
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -8,6 +6,7 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 from watchlist import app
+from gevent.pywsgi import WSGIServer
 
-
-
+http_server = WSGIServer(("0.0.0.0", 5000), app)
+http_server.serve_forever()
